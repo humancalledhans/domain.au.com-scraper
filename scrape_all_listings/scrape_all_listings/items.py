2892e063ccc627d2dedaafdcb8cb9ec5e01f650c
yezeahew <yezeahew@github.com>
2022-11-30 13:35:12 +0800

    initial push

--- scrape_all_listings/scrape_all_listings/items.py
+++ scrape_all_listings/scrape_all_listings/items.py
@@ -6,11 +6,17 @@
 import scrapy
 
 
-class ScrapeAllListingsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# class ScrapeAllListingsItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
 
 class Listing(scrapy.Item):
     listing_title = scrapy.Field
     listing_address = scrapy.Field
+    listing_bedroom_count = scrapy.Field
+    listing_bathroom_count = scrapy.Field
+    listing_parking_count = scrapy.Field
+    listing_unit_size = scrapy.Field
+    listing_unit_type = scrapy.Field
+    listing_full_description = scrapy.Field
--- scrape_all_listings/scrape_all_listings/spiders/domain_spider.py
+++ scrape_all_listings/scrape_all_listings/spiders/domain_spider.py
@@ -13,7 +13,10 @@ class DomainSpiderSpider(scrapy.spiders.SitemapSpider):
     BASE_DOMAIN = "domain.com.au"
     name = 'domain_spider'
     allowed_domains = ['domain.com.au']
-    sitemap_urls = ['https://www.domain.com.au/sitemap-listings-sale-2022111004.xml','https://www.domain.com.au/sitemap-listings-rent-2022112404.xml','https://www.domain.com.au/new-homes-sitemap1.xml']
+
+    # only 3 of the urls are accessible, the other two links to compressed file (download required)
+    # sitemap_urls = ['https://www.domain.com.au/sitemap-listings-sale-2022111004.xml','https://www.domain.com.au/sitemap-listings-rent-2022112404.xml','https://www.domain.com.au/new-homes-sitemap1.xml']
+    sitemap_urls = ['https://www.domain.com.au/sitemap-listings-sale-2022111004.xml']
 
     sitemap_rules = [
         (re.compile(LISTINGS_REGEX), 'parse_listing_page'),
@@ -30,35 +33,40 @@ class DomainSpiderSpider(scrapy.spiders.SitemapSpider):
             "//h1[@class='css-164r41r']//text()".get().strip()
         )
 
-        # these 4 share the same xpath, 
-        # to do : figure out how to differenciate them all 
-        bedroom_count = response.xpath (
-            "//span[@class='css-lvv8is']//text()".get().strip()
+        # these 4 share the same xpath attributes - span[@class="css-lvv8is"]
+        # The only difference between them is that they have different text in the next element 
+        # therefore, [parent::] is used to differentiate them  
+        # //span[contains(text(),"Beds")]//parent::span[@class="css-lvv8is"]        
+        # #----------------------------------------------------------------
+        
+        # note: only works when there are NO other recommended properties on the page
+        bedroom_count = response.xpath(
+            "//span[contains(text(),'Beds')]//parent::span[@class='css-lvv8is']//text()".get().strip()
+        )
+        bathroom_count = response.xpath(
+            "//span[contains(text(),'Baths')]//parent::span[@class='css-lvv8is']//text()".get().strip()
         )
 
-        bathroom_count = response.xpath (
-            "//h1[@class='css-164r41r']//text()".get().strip()
+        parking_count = response.xpath(
+            "//span[contains(text(),'Parking')]//parent::span[@class='css-lvv8is']//text()".get().strip()
         )
 
-        car_count = response.xpath (
-            "//h1[@class='css-164r41r']//text()".get().strip()
+        unit_size = response.xpath(
+            "//span[@class='css-lvv8is' and contains(text(),'m²')]//text()".get().strip()
         )
+        #----------------------------------------------------------------
 
-        unit_size = response.xpath (
-            "//h1[@class='css-164r41r']//text()".get().strip()
-        )
-
-        # this share the same path as contact support's phone number
         unit_type = response.xpath(
-            "//span[@class='css-in3yi3']"
+            "//div[@data-testid ='listing-summary-property-type']//span[@class='css-in3yi3']//text()".get().strip()
         )
 
         # description attempt 1
         # this is a content container ?
-        # unable to extract because it is not named, all containers share the same class 
-        description = response.xpath(
-            "//div[@class='noscript-expander-content css-1ij7r2s']"
-        )
+        # unable to extract because there are no other attributes to use 
+        # all containers share the same class attribute
+        #description = response.xpath(
+        #    "//div[@class='noscript-expander-content css-1ij7r2s']"
+        #)
 
         # description attempt 2 
         # can get headline, but cant get the paragraphs,
@@ -67,14 +75,25 @@ class DomainSpiderSpider(scrapy.spiders.SitemapSpider):
             "//h4[@data-testid='listing-details__description-headline']"
         )
 
-        # description_paragraph = ?
+        full_description_list = response.xpath(
+            # try using sibling ?
+        )
+        full_description = ""
 
+        for sentence in full_description_list:
+            if len(sentence) > 0:
+                full_description = full_description + sentence
 
-        
         print(title,address)
 
         yield Listing(
             listing_title=title,
-            listing_address=address
+            listing_address=address,
+            listing_bedroom_count = bedroom_count,
+            listing_bathroom_count = bathroom_count,
+            listing_parking_count = parking_count,
+            listing_unit_size = unit_size,
+            listing_unit_type = unit_type,
+            listing_full_description = full_description    
         )
 
